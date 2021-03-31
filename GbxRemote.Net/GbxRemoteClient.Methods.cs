4f63e9a0fb4aab8739ec4b1868b2fdd99dab1112
snixtho <snixtho@gmail.com>
2021-04-01 01:14:06 +0200

    Client methods

--- GbxRemote.Net/GbxRemoteClient.Methods.cs
+++ GbxRemote.Net/GbxRemoteClient.Methods.cs
@@ -9,6 +9,10 @@ using System.Threading.Tasks;
 
 namespace GbxRemoteNet {
     public partial class GbxRemoteClient {
+        /*
+         * Methods Reference: https://wiki.trackmania.io/en/dedicated-server/XML-RPC/Methods
+         */
+
         #region System Methods
         /// <summary>
         /// Return an array of all available XML-RPC methods on this server.
@@ -341,6 +345,68 @@ namespace GbxRemoteNet {
         public async Task<bool> ChatForwardToLoginAsync(string text, string senderLogin, string destinationLogin) =>
             (bool)XmlRpcTypes.ToNativeValue<bool>(
                 await CallOrFaultAsync("ChatForwardToLogin", text, senderLogin, destinationLogin)
+            );
+        #endregion
+
+        #region Client
+        public async Task<bool> SendNoticeAsync(string message, string playerLogin, int variant) =>
+            (bool)XmlRpcTypes.ToNativeValue<bool>(
+                await CallOrFaultAsync("SendNotice", message, playerLogin, variant)
+            );
+
+        public async Task<bool> SendNoticeToIdAsync(int clientUid, string message, int avatarUid, int variant) =>
+            (bool)XmlRpcTypes.ToNativeValue<bool>(
+                await CallOrFaultAsync("SendNoticeToId", clientUid, message, avatarUid, variant)
+            );
+
+        public async Task<bool> SendNoticeToLoginAsync(string clientLogin, string message, string avatarLogin, int variant) =>
+            (bool)XmlRpcTypes.ToNativeValue<bool>(
+                await CallOrFaultAsync("SendNoticeToLogin", clientLogin, message, avatarLogin, variant)
+            );
+
+        public async Task<bool> SendDisplayManialinkPageAsync(string xml, int timeout, bool autohide) =>
+            (bool)XmlRpcTypes.ToNativeValue<bool>(
+                await CallOrFaultAsync("SendDisplayManialinkPage", xml, timeout, autohide)
+            );
+
+        public async Task<bool> SendDisplayManialinkPageToIdAsync(int clientId, string xml, int timeout, bool autohide) =>
+            (bool)XmlRpcTypes.ToNativeValue<bool>(
+                await CallOrFaultAsync("SendDisplayManialinkPageToId", clientId, xml, timeout, autohide)
+            );
+
+        public async Task<bool> SendDisplayManialinkPageToLoginAsync(string playerLogin, string xml, int timeout, bool autohide) =>
+            (bool)XmlRpcTypes.ToNativeValue<bool>(
+                await CallOrFaultAsync("SendDisplayManialinkPageToLogin", playerLogin, xml, timeout, autohide)
+            );
+
+        public async Task<bool> SendHideManialinkPageAsync() =>
+            (bool)XmlRpcTypes.ToNativeValue<bool>(
+                await CallOrFaultAsync("SendHideManialinkPage")
+            );
+
+        public async Task<bool> SendHideManialinkPageToIdAsync(int clientId) =>
+            (bool)XmlRpcTypes.ToNativeValue<bool>(
+                await CallOrFaultAsync("SendHideManialinkPageToId", clientId)
+            );
+
+        public async Task<bool> SendHideManialinkPageToLoginAsync(string playerLogin) =>
+            (bool)XmlRpcTypes.ToNativeValue<bool>(
+                await CallOrFaultAsync("SendHideManialinkPageToLogin", playerLogin)
+            );
+
+        public async Task<ManialinkPageAnswerStruct[]> GetManialinkPageAnswersAsync() =>
+            (ManialinkPageAnswerStruct[])XmlRpcTypes.ToNativeValue<ManialinkPageAnswerStruct>(
+                await CallOrFaultAsync("GetManialinkPageAnswers")
+            );
+
+        public async Task<bool> SendOpenLinkToId(int clientId, string url, int linkType) =>
+            (bool)XmlRpcTypes.ToNativeValue<bool>(
+                await CallOrFaultAsync("SendOpenLinkToId", clientId, url, linkType)
+            );
+
+        public async Task<bool> SendOpenLinkToLogin(string playerLogin, string url, int linkType) =>
+            (bool)XmlRpcTypes.ToNativeValue<bool>(
+                await CallOrFaultAsync("SendOpenLinkToId", playerLogin, url, linkType)
             );
         #endregion
     }
---
+++ GbxRemote.Net/Structs/ManialinkPageAnswerStruct.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GbxRemoteNet.Structs {
+    public class ManialinkPageAnswerStruct {
+        public string Login;
+        public int PlayerId;
+        public int Result;
+    }
+}
