1b743527106a9c002e075562d1ae32c74a8a661e
snixtho <snixtho@gmail.com>
2021-04-02 16:57:23 +0200

    Add player methods.

--- GbxRemote.Net/GbxRemoteClient.Methods.cs
+++ GbxRemote.Net/GbxRemoteClient.Methods.cs
@@ -493,6 +493,111 @@ namespace GbxRemoteNet {
             (bool)XmlRpcTypes.ToNativeValue<bool>(
                 await CallOrFaultAsync("Kick", login, message)
             );
+
+        public async Task<bool> KickIdAsync(int id, string message = null) =>
+            (bool)XmlRpcTypes.ToNativeValue<bool>(
+                await CallOrFaultAsync("KickId", id, message)
+            );
+
+        public async Task<bool> BanAsync(string login, string message = null) =>
+            (bool)XmlRpcTypes.ToNativeValue<bool>(
+                await CallOrFaultAsync("Ban", login, message)
+            );
+
+        public async Task<bool> BanAndBlackListAsync(string login, string message, string saveToFile=null) =>
+            (bool)XmlRpcTypes.ToNativeValue<bool>(
+                await CallOrFaultAsync("BanAndBlackList", login, message, saveToFile)
+            );
+
+        public async Task<bool> BanIdAsync(int id, string message) =>
+            (bool)XmlRpcTypes.ToNativeValue<bool>(
+                await CallOrFaultAsync("BanId", id, message)
+            );
+
+        public async Task<bool> UnBanAsync(string login) =>
+            (bool)XmlRpcTypes.ToNativeValue<bool>(
+                await CallOrFaultAsync("UnBan", login)
+            );
+
+        public async Task<bool> CleanBanListAsync() =>
+            (bool)XmlRpcTypes.ToNativeValue<bool>(
+                await CallOrFaultAsync("CleanBanList")
+            );
+
+        public async Task<BanListEntryStruct[]> GetBanListAsync(int maxInfos, int startIndex) =>
+            (BanListEntryStruct[])XmlRpcTypes.ToNativeValue<BanListEntryStruct>(
+                await CallOrFaultAsync("GetBanList", maxInfos, startIndex)
+            );
+
+        public async Task<bool> BlackListAsync(string login) =>
+            (bool)XmlRpcTypes.ToNativeValue<bool>(
+                await CallOrFaultAsync("BlackList", login)
+            );
+
+        public async Task<bool> BlackListIdAsync(int id) =>
+            (bool)XmlRpcTypes.ToNativeValue<bool>(
+                await CallOrFaultAsync("BlackListId", id)
+            );
+
+        public async Task<bool> UnBlackListAsync(string login) =>
+            (bool)XmlRpcTypes.ToNativeValue<bool>(
+                await CallOrFaultAsync("UnBlackList", login)
+            );
+
+        public async Task<BlackListEntryStruct[]> GetBlackListAsync(int maxInfos, int startIndex) =>
+            (BlackListEntryStruct[])XmlRpcTypes.ToNativeValue<BlackListEntryStruct>(
+                await CallOrFaultAsync("GetBanList", maxInfos, startIndex)
+            );
+
+        public async Task<bool> LoadBlackListAsync(string fileName) =>
+            (bool)XmlRpcTypes.ToNativeValue<bool>(
+                await CallOrFaultAsync("LoadBlackList", fileName)
+            );
+
+        public async Task<bool> SaveBlackListAsync(string fileName) =>
+            (bool)XmlRpcTypes.ToNativeValue<bool>(
+                await CallOrFaultAsync("SaveBlackList", fileName)
+            );
+
+        public async Task<bool> AddGuestAsync(string login) =>
+            (bool)XmlRpcTypes.ToNativeValue<bool>(
+                await CallOrFaultAsync("AddGuest", login)
+            );
+
+        public async Task<bool> AddGuestIdAsync(int id) =>
+            (bool)XmlRpcTypes.ToNativeValue<bool>(
+                await CallOrFaultAsync("AddGuestId", id)
+            );
+
+        public async Task<bool> RemoveGuestAsync(string login) =>
+            (bool)XmlRpcTypes.ToNativeValue<bool>(
+                await CallOrFaultAsync("RemoveGuest", login)
+            );
+
+        public async Task<bool> RemoveGuestIdAsync(int id) =>
+            (bool)XmlRpcTypes.ToNativeValue<bool>(
+                await CallOrFaultAsync("RemoveGuestId", id)
+            );
+
+        public async Task<bool> CleanGuestListAsync() =>
+            (bool)XmlRpcTypes.ToNativeValue<bool>(
+                await CallOrFaultAsync("CleanGuestList")
+            );
+
+        public async Task<GuestListEntryStruct[]> GuestListEntryStructAsync(int maxInfos, int startIndex) =>
+            (GuestListEntryStruct[])XmlRpcTypes.ToNativeValue<GuestListEntryStruct>(
+                await CallOrFaultAsync("GuestListEntryStruct", maxInfos, startIndex)
+            );
+
+        public async Task<bool> LoadGuestListAsync(string fileName) =>
+            (bool)XmlRpcTypes.ToNativeValue<bool>(
+                await CallOrFaultAsync("LoadGuestList", fileName)
+            );
+
+        public async Task<bool> SaveGuestListAsync(string fileName) =>
+            (bool)XmlRpcTypes.ToNativeValue<bool>(
+                await CallOrFaultAsync("SaveGuestList", fileName)
+            );
         #endregion
     }
 }
---
+++ GbxRemote.Net/Structs/BanListEntryStruct.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GbxRemoteNet.Structs {
+    public class BanListEntryStruct {
+        public string Login;
+        public string ClientName;
+        public string IPAddress;
+    }
+}
