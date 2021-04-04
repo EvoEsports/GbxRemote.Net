9e5ed3773cc6df0867712a0d02129995f3381576
snixtho <snixtho@gmail.com>
2021-04-04 11:58:06 +0200

    Add missing usings.

--- GbxRemote.Net/GbxRemoteClient.Methods.Script.cs
+++ GbxRemote.Net/GbxRemoteClient.Methods.Script.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using GbxRemoteNet.Structs;
+using GbxRemoteNet.XmlRpc;
+using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
