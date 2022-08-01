b354e9135c94a81dd32e30e2b0fd5d80a1024173
snixtho <snixtho@gmail.com>
2022-08-01 13:17:56 +0200

    Deprecate LadderServerLimits

--- GbxRemote.Net/Structs/LadderServerLimits.cs
+++ GbxRemote.Net/Structs/LadderServerLimits.cs
@@ -1,4 +1,7 @@
-﻿namespace GbxRemoteNet.Structs {
+﻿using System;
+
+namespace GbxRemoteNet.Structs {
+    [Obsolete]
     public class LadderServerLimits {
         public double LadderServerLimitMin { get; set; }
         public double LadderServerLimitMax { get; set; }
