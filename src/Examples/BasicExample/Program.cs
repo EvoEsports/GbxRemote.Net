2982c960fdad8ed34b5000c267e69e084b6eb1e1
snixtho <snixtho@gmail.com>
2022-10-03 10:32:14 +0200

    Update Program.cs

--- src/Examples/BasicExample/Program.cs
+++ src/Examples/BasicExample/Program.cs
@@ -1,26 +1,38 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Examples.Common;
 using GbxRemoteNet;
+using Microsoft.Extensions.Logging;
 
-// create client instance
-GbxRemoteClient client = new("127.0.0.2", 5001);
+namespace BasicExample;
 
-// connect and login
-if (!await client.LoginAsync("SuperAdmin", "SuperAdmin"))
+internal class Program
 {
-    Console.WriteLine("Failed to login.");
-    return;
+    private static async Task Main(string[] args)
+    {
+        // create client instance
+        GbxRemoteClient client = new("127.0.0.2", 5001, Logger.New<Program>(LogLevel.Debug));
+
+        // connect and login
+        if (!await client.LoginAsync("SuperAdmin", "SuperAdmin"))
+        {
+            await client.GetLastConnectionErrorMessageAsync();
+            Console.WriteLine("Failed to login.");
+            return;
+        }
+
+        Console.WriteLine("Connected and authenticated!");
+
+        // get player list
+        var players = await client.GetPlayerListAsync();
+
+        foreach (var player in players)
+        {
+            var info = await client.GetDetailedPlayerInfoAsync(player.Login);
+            Console.WriteLine($"Login: {player.Login}, NickName: {player.NickName}");
+        }
+
+        // disconnect and clean up
+        await client.DisconnectAsync();
+    }
 }
-
-Console.WriteLine("Connected and authenticated!");
-
-// get player list
-var players = await client.GetPlayerListAsync();
-
-foreach (var player in players)
-{
-    var info = await client.GetDetailedPlayerInfoAsync(player.Login);
-    Console.WriteLine($"Login: {player.Login}, NickName: {player.NickName}");
-}
-
-// disconnect and clean up
-await client.DisconnectAsync();
